46e1727d51f6c527314c4a064b1d03a45a5e2c0b
y-matsuda <matsuda@lab.acs-jp.com>
2016-07-28 15:50:36 +0900

    test

--- lib/bbb.rb
+++ lib/bbb.rb
@@ -41,7 +41,7 @@ class Bbb
     doc = REXML::Document.new(isMeetingRunning)
     if doc.root.elements['returncode'].text == "SUCCESS"
       @running = doc.root.elements['running'].to_s.downcase == 'true'
-      Rails.logger.info "meeting #{@running}"
+      Rails.logger.info "meeting #{doc.root.elements['running']} => #{@running}"
     end
 
     if @running
